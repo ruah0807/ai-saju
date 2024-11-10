ba2e071bce9492bd1228f5887b49056782423b7b
Ruah Kim <kimhk0315@gmail.com>
2024-11-10 23:51:53 +0900

    init 수정

--- init.py
+++ init.py
@@ -1,9 +1,11 @@
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
-api_key = os.environ.get("OPENAI_API_KEY")
+# api_key = os.environ.get("OPENAI_API_KEY")
+api_key = st.secrets.get("OPENAI_API_KEY")
 # Open ai API를 사용하기 위한 클라이언트 객체생성
 client = OpenAI(api_key=api_key)
